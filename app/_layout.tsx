1e92d2559e0a5dc751c7250cbecddb21fb43b486
ts-atike <work.aishwaryatike@gmail.com>
2026-07-07 23:39:15 +0530

    Splash Screen setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,22 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  return <Stack />;
+
+useEffect(() => {
+    async function prepare() {
+      // Wait for 3 seconds
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+
+      // Hide the splash screen
+      await SplashScreen.hideAsync();
+    }
+
+    prepare();
+  }, []);
+
+  return <Stack screenOptions={{ headerShown: false}} />;
 }
